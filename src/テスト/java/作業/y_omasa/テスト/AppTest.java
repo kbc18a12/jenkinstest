97be50a4a4cdeb9fb81773c3c4dbfaa77cee857e
kbc18a12 <57973873+kbc18a12@users.noreply.github.com>
2020-07-31 12:02:58 +0900

    Update AppTest.java

--- src/テスト/java/作業/y_omasa/テスト/AppTest.java
+++ src/テスト/java/作業/y_omasa/テスト/AppTest.java
@@ -10,7 +10,7 @@ import org.junit.Test;
 
 public class AppTest {
 
-    // メールアドレスを正しい書式で設定し、同じ値を取得する
+   
     @Test
     public void setEmailWithCollectFormatAndGetMatchingEmail() {
         User user = new User();
@@ -22,14 +22,14 @@ public class AppTest {
         assertThat(user.getEmail(), is("test@example.com"));
     }
 
-    // メールアドレスを誤った書式で設定した場合、例外が発生する
-    @Test(expected =InputCheckException.class)
+   
+    @Test(expected = InputCheckException.class)
     public void setEmailWithWrongFormatResultException() throws Exception{
         User user = new User();
         user.setEmail("testexample.com");
     }
 
-    //パスワードのハッシュを設定し、正しいパスワードをチェックするとtrue
+  
     @Test
     public void setPasswordHashAndCheckCollectPasswordReturnTrue()
     {
@@ -37,22 +37,22 @@ public class AppTest {
         User user = new User();
         MessageDigest digest = null;
         try {
-            //渡された文字列からハッシュを生成 
+           
 			digest = MessageDigest.getInstance("MD5");
 			digest.reset();
 			digest.update(pwd.getBytes("utf8"));
         } catch (Exception e) {
             e.printStackTrace();
         }
-        //生成されたハッシュ値を文字列に変換
+       
         String hashedPw = String.format("%032x", new BigInteger(1, digest.digest()));
-        // userにパスワードハッシュを設定
+        
         user.setPwdHash(String.valueOf(hashedPw));        
 
         assertTrue( user.isPwdValid("1234") );
     }
 
-    //パスワードのハッシュを設定し、誤ったパスワードをチェックするとfalse
+    
     @Test
     public void setPasswordHashAndCheckWrongPasswordReturnFalse()
     {
@@ -60,16 +60,16 @@ public class AppTest {
         String pwd = "1234";
         MessageDigest digest = null;
         try {
-            //渡された文字列からハッシュを生成 
+           
 			digest = MessageDigest.getInstance("MD5");
 			digest.reset();
 			digest.update(pwd.getBytes("utf8"));
         } catch (Exception e) {
             e.printStackTrace();
         }
-        //生成されたハッシュ値を文字列に変換
+       
         String hashedPw = String.format("%032x", new BigInteger(1, digest.digest()));
-        // userにパスワードハッシュを設定
+        
         user.setPwdHash(String.valueOf(hashedPw));        
 
         assertFalse( user.isPwdValid("2345") );
